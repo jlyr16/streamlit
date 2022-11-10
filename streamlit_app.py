6c4a04ce9b9a3872676e44d5a1dc80b257283f48
jlyr16 <111680658+jlyr16@users.noreply.github.com>
2022-11-10 23:29:39 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+#create streamlit app
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#load data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv')
+
+#set title
+st.title('Apple Stock Price')
+
+#set subheader
+st.subheader('Apple Stock Price Data')
+
+#show data
+st.write(df)
+
+#show data as chart
+st.line_chart(df.Close)
+st.line_chart(df.High)
+st.line_chart(df.Low)
+
+#show data as table
+st.table(df)
+
